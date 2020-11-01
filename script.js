a2ad55d26914a342c378fe32f54c02819a5cc2a2
Craig Macleod <c92macleod@gmail.com>
2020-11-01 14:10:24 +0100

    Added text fade animation and text content placeholders

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const GRAY_ANIMATION_DEFITION = 'iconClickGrey 2s forwards'
 const WHITE_ANIMATION_DEFINITION = 'iconClickWhite 2.5s forwards'
 const ICON_ANIMATION_DEFINITION = 'iconClick 0.4s forwards'
+const ICON_TEXT_ANIMATION_DEFINITION = `iconClickText 4s forwards`
 
 function applyIconClickAnimation(sectionID) {
     const graySection = document.querySelector(`${sectionID} .sectionGray`)
     const whiteSection = document.querySelector(`${sectionID} .sectionWhite`)
     const gradientSection = document.querySelector(`${sectionID} .sectionGradient`)
     const icon = document.querySelector(`${sectionID} .icon`)
+    const p = document.querySelector(`${sectionID} p`)
 
     gradientSection.style['z-index'] = '-4'
     graySection.style.display = 'block'
     whiteSection.style.display = 'block'
+    p.style.display = 'block'
+    p.style.animation = ICON_TEXT_ANIMATION_DEFINITION
     icon.style.animation = ICON_ANIMATION_DEFINITION
     graySection.style.animation = GRAY_ANIMATION_DEFITION
     whiteSection.style.animation = WHITE_ANIMATION_DEFINITION
