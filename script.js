33db69eaeaa12337f07299599036bdb3e4ddf3ca
Craig Macleod <c92macleod@gmail.com>
2020-11-01 13:24:59 +0100

    Added logo click animations

---
+++ script.js
@@ -0,0 +1,23 @@
+const GRAY_ANIMATION_DEFITION = 'iconClickGrey 2s forwards'
+const WHITE_ANIMATION_DEFINITION = 'iconClickWhite 2.5s forwards'
+const ICON_ANIMATION_DEFINITION = 'iconClick 0.4s forwards'
+
+function applyIconClickAnimation(sectionID) {
+    const graySection = document.querySelector(`${sectionID} .sectionGray`)
+    const whiteSection = document.querySelector(`${sectionID} .sectionWhite`)
+    const gradientSection = document.querySelector(`${sectionID} .sectionGradient`)
+    const icon = document.querySelector(`${sectionID} .icon`)
+
+    gradientSection.style['z-index'] = '-4'
+    graySection.style.display = 'block'
+    whiteSection.style.display = 'block'
+    icon.style.animation = ICON_ANIMATION_DEFINITION
+    graySection.style.animation = GRAY_ANIMATION_DEFITION
+    whiteSection.style.animation = WHITE_ANIMATION_DEFINITION
+    setTimeout(() => {
+        gradientSection.style['z-index'] = '-6'
+        graySection.style.display = 'none'
+        whiteSection.style.display = 'none'
+    },2000)
+    return
+}
