51b63e2ba2537b4682f2762772732012c73b5887
Craig Macleod <c92macleod@gmail.com>
2020-11-06 19:00:48 +0100

    Added github and codewars footer logos, & did minor code cleaning.

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const GRAY_ANIMATION_DEFITION = 'iconClickGrey 2s forwards'
-const WHITE_ANIMATION_DEFINITION = 'iconClickWhite 2.5s forwards'
+const OUTER_ANIMATION_DEFITION = 'bubbleOuterAnimation 2s forwards'
+const INNER_ANIMATION_DEFINITION = 'bubbleInnerAnimation 2.5s forwards'
 const ICON_ANIMATION_DEFINITION = 'iconClick 0.4s forwards'
 const ICON_TEXT_ANIMATION_DEFINITION = `iconClickText 4s forwards`
 
 function applyIconClickAnimation(sectionID) {
-    const graySection = document.querySelector(`${sectionID} .sectionGray`)
-    const whiteSection = document.querySelector(`${sectionID} .sectionWhite`)
-    const gradientSection = document.querySelector(`${sectionID} .sectionGradient`)
+    const outerSection = document.querySelector(`${sectionID} .bubbleOuter`)
+    const innerSection = document.querySelector(`${sectionID} .bubbleInner`)
+    const gradientSection = document.querySelector(`${sectionID} .bubbleGradient`)
     const icon = document.querySelector(`${sectionID} .icon`)
     const text = document.querySelector(`${sectionID} .textContent`)
 
     gradientSection.style['z-index'] = '-4'
-    graySection.style.display = 'block'
-    whiteSection.style.display = 'block'
+    outerSection.style.display = 'block'
+    innerSection.style.display = 'block'
     text.style.display = 'block'
     text.style.animation = ICON_TEXT_ANIMATION_DEFINITION
     icon.style.animation = ICON_ANIMATION_DEFINITION
-    graySection.style.animation = GRAY_ANIMATION_DEFITION
-    whiteSection.style.animation = WHITE_ANIMATION_DEFINITION
+    outerSection.style.animation = OUTER_ANIMATION_DEFITION
+    innerSection.style.animation = INNER_ANIMATION_DEFINITION
     setTimeout(() => {
         gradientSection.style['z-index'] = '-6'
-        graySection.style.display = 'none'
-        whiteSection.style.display = 'none'
+        outerSection.style.display = 'none'
+        innerSection.style.display = 'none'
     },2000)
     return
 }
