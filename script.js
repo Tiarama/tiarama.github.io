d10fae31d17d2817890966be012b1c64fd236765
Craig MacLeod <c92macleod@gmail.com>
2020-12-17 14:47:46 +0100

    changed cursor over certain elements, added image hovers for Passions section, fixed icon animation to display none after 2s

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
 // Icon click / Content reveal animations
 
-const OUTER_ANIMATION_DEFITION = 'bubbleOuterAnimation 2s forwards'
-const INNER_ANIMATION_DEFINITION = 'bubbleInnerAnimation 2.5s forwards'
-const ICON_ANIMATION_DEFINITION = 'iconClick 0.4s forwards'
-const ICON_TEXT_ANIMATION_DEFINITION = `iconClickText 4s forwards`
-
 function applyIconClickAnimation(sectionID) {
     const outerSection = document.querySelector(`${sectionID} .bubbleOuter`)
     const innerSection = document.querySelector(`${sectionID} .bubbleInner`)
@@ -16,14 +11,15 @@ function applyIconClickAnimation(sectionID) {
     outerSection.style.display = 'block'
     innerSection.style.display = 'block'
     text.style.display = 'block'
-    text.style.animation = ICON_TEXT_ANIMATION_DEFINITION
-    icon.style.animation = ICON_ANIMATION_DEFINITION
-    outerSection.style.animation = OUTER_ANIMATION_DEFITION
-    innerSection.style.animation = INNER_ANIMATION_DEFINITION
+    text.style.animation = `iconClickText 4s forwards`
+    icon.style.animation = 'iconClick 0.4s forwards'
+    outerSection.style.animation = 'bubbleOuterAnimation 2s forwards'
+    innerSection.style.animation = 'bubbleInnerAnimation 2.5s forwards'
     setTimeout(() => {
         gradientSection.style['z-index'] = '-6'
         outerSection.style.display = 'none'
         innerSection.style.display = 'none'
+        icon.style.display = 'none'
     },2000)
     return
 }
